4b56736413d2ddd07d01a3e6763e4bc223f2da42
Dion Chang <babogoos@gmail.com>
2025-06-16 04:44:46 +0000

    把這個網站掛在 /emoji-assit 下

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/emoji-assist',
   typescript: {
     ignoreBuildErrors: true,
   },
