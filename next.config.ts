dc476974f42f3dcacfc42c20c848984c50159938
Dion Chang <babogoos@gmail.com>
2025-06-16 09:37:31 +0000

    preview壞了

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/emoji-assist',
   typescript: {
     ignoreBuildErrors: true,
   },
